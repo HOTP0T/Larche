f434d313f5538cb3de3048283efcc51a8b2820c3
HOTPOT <tpcmaky@gmail.com>
2025-02-02 18:15:02 +0100

    reinit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/Larche/',
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  }
 }) 

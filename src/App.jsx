bc0e09bc018e18350281b82e686dc7b87a1864e1
HOTPOT <tpcmaky@gmail.com>
2025-02-02 17:14:37 +0100

    Fix routing for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Larche">
       <div className="app">
         <Routes>
           <Route path="/" element={
